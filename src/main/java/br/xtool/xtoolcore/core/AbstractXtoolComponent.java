f3ae613aafabed07b35f4033ebd95490b753254e
jcruz <jcruz@tre-pa.jus.br>
2020-03-25 21:49:56 -0300

    Ajuste no Console

--- src/main/java/br/xtool/xtoolcore/core/AbstractXtoolComponent.java
+++ src/main/java/br/xtool/xtoolcore/core/AbstractXtoolComponent.java
@@ -1,24 +1,8 @@
 package br.xtool.xtoolcore.core;
 
-import picocli.CommandLine;
-
 /**
  * Classe base para os componentes Xtool.
  */
-public abstract class AbstractXtoolComponent {
-
-    /**
-     * Retorna as opções de linha de comando (picocli).
-     *
-     * @return
-     */
-    public CommandLine.Model.OptionSpec options() {
-        return null;
-    }
-
-    /**
-     * Método que deverá ser implementado pelo componente Xtool.
-     */
-    public abstract void run();
+public abstract class AbstractXtoolComponent implements Runnable {
 
 }
